a2deeb5a5d6f8e992826e3cfd4595de93a5998fb
Arnaud Huck <arnaud.huck2@hotmail.fr>
2021-06-23 18:38:51 +0200

    added three files

--- Book_Scraper.py
+++ Book_Scraper.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import requests
-import re
 from bs4 import BeautifulSoup
+import csv
 
-URL = requests.get('https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
-soup = BeautifulSoup(URL.content, 'html.parser')
+
+def get_data(url):
+    r = requests.get(url)
+    return r.text
+
+
+URL = get_data('https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
+soup = BeautifulSoup(URL, 'html.parser')
 # Finds all images
-book_images = soup.find_all('img', {'src': re.compile('jpg')})
-for book_image in book_images:
-    print(book_image['src']+'\n')
+for item in soup.find_all('img'):
+    book_image = (item['src'])
+
 Book_Data = soup.find(class_='container-fluid page')
 Product_Information = Book_Data.find_all('tr')
 # prints first Item found in Items
@@ -18,22 +24,46 @@ Product_Information = Book_Data.find_all('tr')
 
 # Create an empty dictionary
 book_dictionary = {}
-
-# Finds all 'th' occurrences as a list
+# Finds all 'th' occurrences and set those as a list
 Product_Information_Name = [item.find('th').get_text() for item in Product_Information]
+Product_Information_Name.pop(1)
+Product_Information_Name.pop(3)
+Product_Information_Name.pop(4)
 print(Product_Information_Name)
-# Finds all 'td' occurrences as a list
+# Finds all 'td' occurrences and set those as a list
 Product_Information_Value = [item.find('td').get_text() for item in Product_Information]
+Product_Information_Value.pop(1)
+Product_Information_Value.pop(3)
+Product_Information_Value.pop(4)
 print(Product_Information_Value)
-# Finds first 'p' occurrence in the product_page direct children
+# Finds 'p' occurrences then find in the product_page the first children
 product_page = soup.find(class_='product_page')
 book_description = product_page.find('p', recursive=False).get_text()
 # Finds book's title
 books_title = soup.find(class_='col-sm-6 product_main')
 book_title = books_title.find('h1').get_text()
-book_ratings = books_title.find(class_='star-rating Three')
-book_rating = book_ratings.find_all('i')
-print(book_rating)
+book_ratings = books_title.select('div > p')[1].get_text(strip=True)
+print(book_ratings)
+book_rating = ('1 star', '2 stars', '3 stars', '4 stars', '5 stars')
+
+'''
+if 'One' in book_ratings:
+    book_rating = book_rating[0]
+    print(book_rating)
+if 'Two' in book_ratings:
+    book_rating = book_rating[1]
+    print(book_rating)
+if 'Three' in book_ratings:
+    book_rating = book_rating[2]
+    print(book_rating)
+if 'Four' in book_ratings:
+    book_rating = book_rating[3]
+    print(book_rating)
+if 'Five' in book_ratings:
+    book_rating = book_rating[4]
+    print(book_rating)
+'''
+
 # Finds book's category
 book_profile = soup.find(class_='breadcrumb')
 book_categories = book_profile.find_all('li')
@@ -51,8 +81,9 @@ Product_Information_Name.append('Category')
 book_dictionary['Category'] = book_category
 Product_Information_Name.append('Rating')
 book_dictionary['Rating'] = book_rating
-Product_Information_Name.append('Image Link')
-book_dictionary['Image '] = book_image
+Product_Information_Name.append('Image_Link')
+book_dictionary['Image_Link'] = ()
+
 Books_Features = pd.DataFrame([book_dictionary])
 
 print(Books_Features)
