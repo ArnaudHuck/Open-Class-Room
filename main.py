f74058223a6e57869404f7b4a25fb728853c9bee
Arnaud Huck <arnaud.huck2@hotmail.fr>
2022-01-24 18:34:29 +0100

    added main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import csv
 import os
+import sys
 from scrap_category_url import get_category_urls, next_page_url, scrap_category_books, export_list_to_csv, export_single_book_to_csv
 from typing import Dict, List, Tuple, Any
 from Book_Scraper import scrap_book_url
+import os.path
 
 
 base_url = 'https://books.toscrape.com/index.html'
@@ -34,6 +36,22 @@ def export_category_dictionaries_and_images(url: str, category_name: str) -> Lis
     export_list_to_csv(scrap_category_books(url), category_name)
 
 
-# export_dictionary_and_image('https://books.toscrape.com/catalogue/frankenstein_20/index.html', "Frankenstein")
-# export_category_dictionaries_and_images('https://books.toscrape.com/catalogue/category/books/mystery_3/index.html', 'mystery.csv')
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) == 3:
+        if args[0] == 'category':
+            export_category_dictionaries_and_images(args[1], args[2])
+        elif args[0] == 'book':
+            export_dictionary_and_image(args[1], args[2])
+        else:
+            print("Invalid parameters")
+    elif len(args) == 1:
+        if args[0] == 'all':
+            export_categories_dictionaries_and_images()
+    else:
+        print("Invalid parameters")
+
+
+# export_dictionary_and_image('https://books.toscrape.com/catalogue/frankenstein_20/index.html', "Frankenstein.csv")
+# export_category_dictionaries_and_images('https://books.toscrape.com/catalogue/category/books/mystery_3/index.html',mystery.csv 'm')
 # export_categories_dictionaries_and_images()
